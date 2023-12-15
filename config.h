634a70316ac215b1dc3e23fe6b2d2f2d61ac2851
Adrian Christian Santos <adrian.crsantos@gmail.com>
2023-12-15 18:47:07 +0800

    Change: font & opacity

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font =
-    "JetBrainsMono Nerd Font:pixelsize=16:antialias=true:autohint=true";
+    "JetBrainsMono NF:pixelsize=16:antialias=true:autohint=true";
 static char *font2[] = {"JetBrainsMono Nerd Font:pixelsize=16"};
 static int borderpx = 0;
 
@@ -108,7 +108,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.9;
+float alpha = 1.0;
 float alphaOffset = 0.0;
 float alphaUnfocus;
 
