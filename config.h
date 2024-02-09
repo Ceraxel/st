795a94e0760271e969b10d4b4b6a5f51554b24a6
Adrian Christian Santos <adrian.crsantos@gmail.com>
2024-02-09 10:06:26 +0800

    change font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono NF:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Berkeley Mono:pixelsize=16:antialias=true:autohint=true";
 static char *font2[] = {"JetBrainsMono Nerd Font:pixelsize=16"};
 static int borderpx = 0;
 
